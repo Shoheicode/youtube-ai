203c442fc5d03f2bc81198ef1be72d3273cee118
ShoheiCode <ucodejasonirie@gmail.com>
2025-03-31 10:47:55 -0700

    update python page

--- PythonServer/app.py
+++ PythonServer/app.py
@@ -58,8 +58,9 @@ def upload_video():
 
     print("Query received:", query)
     appearances = []
-    appearances.append({"name": "test", "start": 0, "end": 10})
-    appearances.append({"name": "test2", "start": 10, "end": 20})
+    appearances.append({"name": "test", "start": 0, "end": 10, "highlights": generate_simulated_highlights("video_id")})
+    appearances.append({"name": "test2", "start": 10, "end": 20, "highlights": generate_simulated_highlights("video_id")})
+    appearances.append({"name": "test3", "start": 20, "end": 30, "highlights": generate_simulated_highlights("video_id")})
     return jsonify({"appearances": appearances})
 
     # if "video" not in request.files:
