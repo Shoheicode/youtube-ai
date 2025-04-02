4cd9793fa562fff1f0ae68a9bb08021f5b4d9308
ShoheiCode <ucodejasonirie@gmail.com>
2025-04-01 22:48:46 -0700

    update

--- src/app/firebase/firebase.js
+++ src/app/firebase/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,4 +19,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getFirestore(app);
 
-export {app, database}
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export {app, database, auth}
