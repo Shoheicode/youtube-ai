7c5b3469cf77c3b96ca05389a2394eea5f96bff8
ShoheiCode <ucodejasonirie@gmail.com>
2025-04-01 17:39:38 -0700

    checking for audio transcript

--- PythonServer/audio_to_transcript.py
+++ PythonServer/audio_to_transcript.py
@@ -1,3 +1,4 @@
+from faster_whisper import WhisperModel
 import whisper
 import subprocess
 import time
@@ -12,7 +13,7 @@ def format_time(seconds):
     return f"{int(hours):02d}:{int(minutes):02d}:{seconds:.2f}"
 
 
-def audio_to_transcript(audio_file):
+def audio_to_transcript_whisper(audio_file):
     """
     Convert audio to transcript using Whisper model.
 
@@ -24,6 +25,39 @@ def audio_to_transcript(audio_file):
     """
     # Load the Whisper model
     model = whisper.load_model("medium")
+
+    # Transcribe the audio file
+    result = model.transcribe(audio_file, word_timestamps=True)
+
+    output_string = ""
+
+    # Access segments with timestamps
+    for segment in result["segments"]:
+        start_time = segment["start"]
+        end_time = segment["end"]
+        text = segment["text"]
+        output_string += f"[{start_time:.2f}s -> {end_time:.2f}s] {text}\n"
+        # print(f"[{format_time(start_time)} --> {format_time(end_time)}] {text}")
+
+    # Extract and return the transcript
+    return output_string
+
+
+def audio_to_transcript_fast_whisper(audio_file):
+    """
+    Convert audio to transcript using Whisper model.
+
+    Args:
+        audio_file (str): Path to the audio file.
+
+    Returns:
+        str: Transcript of the audio.
+    """
+    # Choose model: tiny, base, small, medium, large-v2
+    model_size = "medium"
+
+    # Set compute_type to "int8" for max speed on CPU, or "float16" for GPU
+    model = WhisperModel(model_size, compute_type="int8", device="cpu")
 
     # Transcribe the audio file
     result = model.transcribe(audio_file, word_timestamps=True)
@@ -56,7 +90,7 @@ convert_to_wav(path, wav_path)
 print("FOR mp3")
 start_time = time.time()
 
-print(audio_to_transcript(path))
+print(audio_to_transcript_whisper(path))
 
 end_time = time.time()
 
@@ -66,7 +100,7 @@ print(f"Transcription took {elapsed_time:.2f} seconds")
 print("FOR WAV")
 start_time = time.time()
 
-print(audio_to_transcript(wav_path))
+print(audio_to_transcript_fast_whisper(path))
 
 end_time = time.time()
 
