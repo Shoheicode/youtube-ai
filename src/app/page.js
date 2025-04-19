8b6b243292aa096794e5a7c444490ef8b1884813
ShoheiCode <ucodejasonirie@gmail.com>
2025-04-19 03:04:36 -0700

    update signed out

--- src/app/page.js
+++ src/app/page.js
@@ -56,7 +56,7 @@ export default function Home() {
         <div className="bg-blue-600 p-4">
           {user ?(
             <>
-            <p>Logged in</p>
+            <p>Sign out</p>
            </>
           ): <>
             <LoginButton/>
